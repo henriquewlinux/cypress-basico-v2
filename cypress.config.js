88272ffff43bafc82aefa98b2822323201b0729e
Henrique Silveira <henrique.silveira@tech4h.com.br>
2022-12-08 14:18:43 -0300

    Lesson Eight - Done

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,9 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+    }
     //pluginsFile: false,
-    viewportHeight: 880,
-    viewportWidth: 1280
+    //viewportHeight: 880,
+    //viewportWidth: 1280
   }
 })
